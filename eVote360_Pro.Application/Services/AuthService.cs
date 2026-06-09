30b7c3fa6e3639f4033017a76390453e90037cce
IsaiasMorillo <isaiasmorillo5556@gmail.com>
2026-06-08 22:27:05 -0400

    feat: add AuthService and PoliticalPartyService classes

---
+++ eVote360_Pro.Application/Services/AuthService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eVote360_Pro.Application.Services
+{
+    internal class AuthService
+    {
+    }
+}
